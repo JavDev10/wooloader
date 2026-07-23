2d621fff1012853a4b02f834a6e3e4c9ea224183
JavDev10 <javi2233as@gmail.com>
2026-07-22 22:58:08 -0400

    change email

--- src/routes/legal/legalMeta.ts
+++ src/routes/legal/legalMeta.ts
@@ -4,6 +4,6 @@ export const LEGAL_LAST_UPDATED = '22 de julio de 2026'
 
 // TODO: replace with the email address you want publicly reachable for
 // privacy/legal requests before deploying the hosted version.
-export const CONTACT_EMAIL = 'contacto@example.com'
+export const CONTACT_EMAIL = 'j.camanotorres12@gmail.com'
 
 export const REPO_URL = 'https://github.com/JavDev10/wooloader'
